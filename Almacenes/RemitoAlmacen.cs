32bf55b7b0a69a056fc70d671bff97b396b042d1
dalon <dalon@DESKTOP-S6LUNGD>
2024-11-06 20:40:13 -0300

    Cambios Almacenes Transportista, Remito y MercaderíaEnStock.

--- Almacenes/RemitoAlmacen.cs
+++ Almacenes/RemitoAlmacen.cs
@@ -1,4 +1,28 @@
-﻿namespace Pampazon.Almacenes;
+﻿using System.Text.Json;
+
+namespace Pampazon.Almacenes;
 public static class RemitoAlmacen
 {
+    private static List<RemitoEnt> _remitos = new();
+    public static IReadOnlyCollection<RemitoEnt> Remitos => _remitos.AsReadOnly();
+
+    public static void Grabar()
+    {
+
+        var datos = JsonSerializer.Serialize(_remitos);
+
+        File.WriteAllText("Remitos.json", datos);
+    }
+
+    public static void Leer()
+    {
+        if (!File.Exists("Remitos.json"))
+        {
+            return;
+        }
+
+        var datos = File.ReadAllText("Remitos.json");
+
+        _remitos = JsonSerializer.Deserialize<List<RemitoEnt>>(datos)!;
+    }
 }
