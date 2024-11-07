1480025a799a6d3c2e4118af2ed3e7065a480b39
aruyin96 <missariimisakii.1.12@gmail.com>
2024-11-06 21:21:54 -0300

    Van los cambios en almacen de orden de entregaaa

--- Almacenes/OrdenDeEntregaAlmacen.cs
+++ Almacenes/OrdenDeEntregaAlmacen.cs
@@ -1,4 +1,29 @@
-﻿namespace Pampazon.Almacenes;
+﻿using System.Text.Json;
+
+namespace Pampazon.Almacenes;
 public static class OrdenDeEntregaAlmacen
 {
+    private static List<OrdenDeEntregaEnt> _ordenesDeEntrega = new();
+    public static IReadOnlyCollection<OrdenDeEntregaEnt> ordenesDeEntrega => _ordenesDeEntrega.AsReadOnly();
+
+    public static void Grabar()
+    {
+        var datos = JsonSerializer.Serialize(_ordenesDeEntrega);
+
+        //Grabarlo en disco:
+        File.WriteAllText("OrdenesDeEntrega.json", datos);
+    }
+
+    public static void Leer()
+    {
+        if (!File.Exists("OrdenesDeEntrega.json")) 
+        {
+            return;
+        }
+
+        var datos = File.ReadAllText("OrdenesDeEntrega.json");
+
+        _ordenesDeEntrega = JsonSerializer.Deserialize<List<OrdenDeEntregaEnt>>(datos)!;
+    }
+
 }
