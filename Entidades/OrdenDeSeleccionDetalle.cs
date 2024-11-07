efd756f0257cc9aa6e735c8873fd28e0bb9a53ee
DAIANA SELENE PINHEIRO <15PI41552828@campus.economicas.uba.ar>
2024-11-06 21:36:25 -0300

    Modificaciones sobre entidad Orden de Selección

--- Entidades/OrdenDeSeleccionDetalle.cs
+++
@@ -1,6 +0,0 @@
-
-public class OrdenDeSeleccionDetalle
-{
-    public string SKU { get; set; }
-    public int Cantidad { get; set; }
-}
--- Entidades/OrdenDeSeleccionEnt.cs
+++ Entidades/OrdenDeSeleccionEnt.cs
@@ -4,5 +4,5 @@ public class OrdenDeSeleccionEnt
     public int NumeroOS { get; set; }
     public List<int> OrdenesDePreparacion { get; set; }
     public OEstadoEnum Estado { get; set; }
-    public List<OrdenDeSeleccionDetalle> Detalle { get; set; }
+    public List<int> Detalle { get; set; }
 }
