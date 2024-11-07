84215e62648d794e775f44764c3e3b9ff184b41b
dalon <dalon@DESKTOP-S6LUNGD>
2024-11-06 21:02:53 -0300

    Cambios Almacenes OrdenDePreparacion y OrdenDeSeleccion.

--- Almacenes/OrdenDeSeleccionAlmacen.cs
+++ Almacenes/OrdenDeSeleccionAlmacen.cs
@@ -1,4 +1,43 @@
-﻿namespace Pampazon.Almacenes;
+﻿using System.Text.Json;
+
+namespace Pampazon.Almacenes;
 public static class OrdenDeSeleccionAlmacen
 {
+    private static List<OrdenDeSeleccionEnt> ordenesSeleccion = [];
+
+    public static IReadOnlyCollection<OrdenDeSeleccionEnt> OrdenesSeleccion => ordenesSeleccion.AsReadOnly();
+
+    public static void Grabar()
+    {
+        var datos = JsonSerializer.Serialize(ordenesSeleccion);
+        File.WriteAllText("OrdenesDeSeleccion.json", datos);
+    }
+
+    public static void Leer()
+    {
+        if (!File.Exists(@"OrdenesDeSeleccion.json"))
+        {
+            return;
+        }
+
+        var datos = File.ReadAllText("OrdenesDeSeleccion.json");
+
+        ordenesSeleccion = JsonSerializer.Deserialize<List<OrdenDeSeleccionEnt>>(datos)!;
+    }
+
+    internal static string Nueva(OrdenDeSeleccionEnt nuevaOrden)
+    {
+        if (OrdenDeSeleccionAlmacen.OrdenesSeleccion.Count == 0)
+        {
+            nuevaOrden.NumeroOS = 1;
+        }
+        else
+        {
+            nuevaOrden.NumeroOS = OrdenDeSeleccionAlmacen.OrdenesSeleccion.Max(o => o.NumeroOS);
+        }
+
+
+        ordenesSeleccion.Add(nuevaOrden);
+        return null;
+    }
 }
